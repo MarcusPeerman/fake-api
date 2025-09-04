1307b92d508e5a2d39463b955e6e19c368e7b679
Marcus Peerman <marcus.peerman@gwu.edu>
2025-09-04 11:33:21 -0400

    fix: use dynamic port for Render

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running and listening on port ${PORT}`);
 });
 
